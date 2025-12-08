837c8ce366f1cca3fa30bb4463a79f68b7f124f6
DaoChaShao <27882272@qq.com>
2025-12-08 21:42:28 +0800

    feat: add neural network architectures module with various models

---
+++ src/nets/__init__.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/11/15 00:17
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   __init__.py
+# @Desc     :   
+
+"""
+****************************************************************
+Neural Nets Module - Neural Network Architectures
+----------------------------------------------------------------
+This module provides a complete set of neural network architectures
+for various machine learning tasks including segmentation, classification,
+and sequence modeling.
+
+Main Categories:
++ Standard4LayersUNetClassification: 4-layer UNet variant for semantic segmentation
++ Standard5LayersUNetForClassification: 5-layer UNet variant for semantic segmentation
++ LSTMRNNForClassification: Recurrent Neural Network for sequence classification tasks
++ NormalRNNForClassification: Standard RNN for sequence classification tasks
+
+Usage:
++ Direct import of models via:
+    - from src.nn import Standard4LayersUNetClassification, LSTMRNNForClassification, etc.
++ Instantiate models with default or custom parameters as needed.
+****************************************************************
+"""
+
+__author__ = "Shawn Yu"
+__version__ = "0.2.0"
+
+from .gru import GRUNet
+from .lstm import LSTMNet
+from .rnn import RNNNet
+from .unet4layers4sem import Standard4LayersUNetClassification
+from .unet5layers4sem import Standard5LayersUNetForClassification
+
+__all__ = [
+    "GRUNet",
+    "LSTMNet",
+    "RNNNet",
+    "Standard4LayersUNetClassification",
+    "Standard5LayersUNetForClassification",
+]
