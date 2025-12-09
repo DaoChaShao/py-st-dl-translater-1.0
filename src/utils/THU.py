d0f9589f0c4aa1a467710313573f12dde7d82c99
DaoChaShao <27882272@qq.com>
2025-12-10 02:12:47 +0800

    feat: implement THULACTokeniser class for text tokenization with optional POS tagging

--- src/utils/THU.py
+++ src/utils/THU.py
@@ -8,50 +8,51 @@
 
 from thulac import thulac
 
-from src.utils.decorator import timer
 
-thulac4pos = None
-thulac4only = None
+class THULACTokeniser:
 
+    def __init__(self, with_pos: bool = False):
+        self._with_pos = with_pos
+        self._thu = None
 
-@timer
-def cut_pos(text: str) -> tuple[list[tuple[str, str]], list[str]]:
-    """ Cut text using THULAC
-    :param text: text to cut
-    :return: list of tuples of cut words and their POS tags
-    """
-    global thulac4pos
-    if thulac4pos is None:
-        thulac4pos = thulac(seg_only=False)
-        print("THUCLAC model loaded. The text will be cut with POS tags.")
+    def __enter__(self):
+        match self._with_pos:
+            case True:
+                self._thu = thulac(seg_only=False)
+                # print("THUCLAC model loaded. The text will be cut with POS tags.")
+            case False:
+                self._thu = thulac(seg_only=True)
+                # print(f"THUCLAC model loaded. The text will be cut ONLY.")
+            case _:
+                raise ValueError("Invalid value for with_pos. Must be True or False.")
 
-    words_tag: list[tuple[str, str]] = thulac4pos.cut(text)
-    words: list[str] = [word for word, tag in words_tag]
+        return self
 
-    print(f"The text has been cut into {len(words)} words using THULAC.")
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self._thu:
+            self._thu = None
 
-    return words_tag, words
+            # print("THUCLAC model released.")
 
+    def tokenise(self, text: str) -> list[str] | list[tuple[str, str]]:
+        if not self._thu:
+            raise RuntimeError("THUCLAC model is not loaded. Please use 'with' statement to load the model.")
 
-# @timer
-def cut_only(text: str) -> list[str]:
-    """ Cut text using THULAC without POS tags
-    :param text: text to cut
-    :return: list of cut words
-    """
-    global thulac4only
-    if thulac4only is None:
-        thulac4only = thulac(seg_only=True)
-        print(f"THUCLAC model loaded. The text will be cut ONLY.")
-
-    words: list[str] = thulac4only.cut(text)
-    # Flatten the list of tuples to get only words
-    words = [word for word, _ in words]
-
-    # print(f"The text has been cut into {len(words)} words using THULAC (without POS tags).")
-
-    return words
+        match self._with_pos:
+            case True:
+                words_tag: list[tuple[str, str]] = self._thu.cut(text)
+                return words_tag
+            case False:
+                words: list[str] = self._thu.cut(text)
+                words = [word for word, _ in words]
+                return words
+            case _:
+                raise ValueError("Invalid value for with_pos. Must be True or False.")
 
 
 if __name__ == "__main__":
-    pass
+    text = "我爱北京天安门。"
+
+    with THULACTokeniser(with_pos=False) as thu:
+        tokens = thu.tokenise(text)
+        print("Tokens:", tokens)
