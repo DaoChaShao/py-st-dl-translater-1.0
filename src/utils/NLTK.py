7eaecb3908e16c87ce9e3dcee0e23e55c8e2482c
DaoChaShao <27882272@qq.com>
2025-12-10 02:12:29 +0800

    feat: add NLTKTokenizer class for text tokenization and detokenization

---
+++ src/utils/NLTK.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/12/9 20:25
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   NLTK.py
+# @Desc     :   
+
+from nltk.tokenize import TreebankWordTokenizer, TreebankWordDetokenizer
+
+
+class NLTKTokenizer:
+    """ NLTK Tokeniser """
+
+    def __init__(self, lang: str = "en", INV: bool = False) -> None:
+        self._lang = lang
+        self._INV = INV
+        self._tokenizer = None
+
+    def __enter__(self):
+        if self._INV:
+            self._tokenizer = TreebankWordDetokenizer()
+        else:
+            self._tokenizer = TreebankWordTokenizer()
+
+        # print(f"NLTK Tokenizer for language '{self._lang}' initialized.")
+
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self._tokenizer:
+            self._tokenizer = None
+            # print(f"NLTK Tokenizer for language '{self._lang}' closed.")
+
+    def tokenize(self, text: str) -> list[str]:
+        """ Tokenize the input text """
+        return self._tokenizer.tokenize(text)
+
+    def detokenize(self, tokens: list[str]) -> str:
+        """ Detokenize the input tokens """
+        return self._tokenizer.detokenize(tokens)
+
+
+if __name__ == "__main__":
+    sample_text = "Hello, world! This is a test."
+
+    with NLTKTokenizer(lang="en") as tokenizer:
+        tokens = tokenizer.tokenize(sample_text)
+        print("Tokens:", tokens)
+
+    with NLTKTokenizer(lang="en", INV=True) as detokenizer:
+        detext = detokenizer.detokenize(tokens)
+        print("Detokenized Text:", detext)
