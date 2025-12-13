12d7e3787fd5e0af055f27545f76be88dd466f3d
DaoChaShao <27882272@qq.com>
2025-12-14 00:56:21 +0800

    feat: implement database connection and enhance model evaluation with BLEU scoring

--- evaluator.py
+++ evaluator.py
@@ -6,32 +6,41 @@
 # @File     :   evaluator.py
 # @Desc     :
 
+from functools import cache
+from numpy import mean as np_mean, max as np_max, min as np_min, median as np_median
 from pathlib import Path
-from random import randint
-from torch import Tensor, load, device, no_grad, nn, argmax
+from random import choice
+from torch import Tensor, tensor, load, device, no_grad, long
 from tqdm import tqdm
 
 from src.configs.cfg_rnn import CONFIG4RNN
-from src.configs.cfg_types import Lang, Tokens, Tasks
-from src.dataloaders.dataloader4torch import TorchDataLoader
-from src.datasets.TS4classification import TimeSeriesTorchDatasetForClassification
-from src.nets.gru4classification import GRUClassifier
+from src.configs.cfg_types import Lang, Tokens, Seq2SeqNet, Seq2SeqStrategies
+from src.nets.seq2seq import SeqToSeqCoder
 from src.utils.helper import Timer
 from src.utils.highlighter import starts, lines, red, green
-from src.utils.nlp import spacy_batch_tokeniser, build_word2id_seqs
+from src.utils.NLTK import bleu_score
+from src.utils.nlp import SpaCyBatchTokeniser, build_word2id_seqs
 from src.utils.SQL import SQLiteIII
 from src.utils.stats import create_full_data_split, load_json
 
 
+@cache
+def connect_db() -> list[tuple]:
+    """ Connect to the Database and Return the Connection Object """
+    table: str = "translater"
+    cols: dict = {"en": str, "cn": str}
+    path: Path = Path(CONFIG4RNN.FILEPATHS.SQLITE)
+
+    with SQLiteIII(table, cols, path) as db:
+        data = db.fetch_all(col_names=[col for col in cols.keys()])
+
+    return data
+
+
 def main() -> None:
     """ Main Function """
-    # Get the data from the database: Method II
-    table: str = "news"
-    cols: dict[str, type[int | str]] = {"label": int, "content": str}
-    with SQLiteIII(table, cols, CONFIG4RNN.FILEPATHS.SQLITE) as db:
-        database = db.fetch_all([col for col in cols.keys()])
-        # print(len(database))
-        # print()
+    # Get the data from the database
+    database: list[tuple] = connect_db()
     # pprint(data[:3])
     # print(len(data))
 
@@ -42,117 +51,127 @@ def main() -> None:
         # Tokenise the data
         # amount: int | None = 100
         amount: int | None = None
-        contents: list[str] = [content for _, content in data]
+        batches: int = 16 if amount else 128
+        cn4prove: list[str] = [c for _, c in data]
+        en4prove: list[str] = [e for e, _ in data]
+        assert len(cn4prove) == len(en4prove), "Chinese and English data length mismatch."
         if amount is None:
-            news: list[list[str]] = spacy_batch_tokeniser(
-                contents, lang=Lang.EN, batches=CONFIG4RNN.PREPROCESSOR.BATCHES
-            )
+            with SpaCyBatchTokeniser(Lang.CN, batches=batches, strict=False) as tokeniser:
+                cn_items: list[list[str]] = tokeniser.batch_tokenise(cn4prove)
+            with SpaCyBatchTokeniser(Lang.EN, batches=batches, strict=False) as tokeniser:
+                en_items: list[list[str]] = tokeniser.batch_tokenise(en4prove)
         else:
-            news: list[list[str]] = spacy_batch_tokeniser(
-                contents[:amount], lang=Lang.EN, batches=CONFIG4RNN.PREPROCESSOR.BATCHES
-            )
-        # print(news)
+            with SpaCyBatchTokeniser(Lang.CN, batches=batches, strict=False) as tokeniser:
+                cn_items: list[list[str]] = tokeniser.batch_tokenise(cn4prove[:amount])
+            with SpaCyBatchTokeniser(Lang.EN, batches=batches, strict=False) as tokeniser:
+                en_items: list[list[str]] = tokeniser.batch_tokenise(en4prove[:amount])
+        # print(cn_items[:3])
+        # print(en_items[:3])
 
-        # Load the dictionary and convert
-        dic: Path = Path(CONFIG4RNN.FILEPATHS.DICTIONARY)
-        dictionary: dict[str, int] = load_json(dic)
+        # Load dictionary
+        dic_cn: Path = Path(CONFIG4RNN.FILEPATHS.DICTIONARY_CN)
+        dictionary_cn: dict = load_json(dic_cn) if dic_cn.exists() else print("Dictionary file not found.")
+        dic_en: Path = Path(CONFIG4RNN.FILEPATHS.DICTIONARY_EN)
+        dictionary_en: dict = load_json(dic_en) if dic_en.exists() else print("Dictionary file not found.")
+        reversed_dict: dict = {idx: word for word, idx in dictionary_en.items()}
+        # print(reversed_dict)
 
-        # Convert sentences to sequence using dictionary
-
-        sequences: list[list[int]] = build_word2id_seqs(news, dictionary, Tokens.UNK)
-
-        # Get the relevant label
-        labels: list[int] = [label for label, _ in data]
-
-        # Set up dataset
-        MAX_SEQ_LEN: int = 48
-        dataset = TimeSeriesTorchDatasetForClassification(
-            sequences, labels,
-            seq_max_len=MAX_SEQ_LEN,
-            pad_token=dictionary[Tokens.PAD]
-        )
-        # starts()
-        # print("Data Preprocessing Summary:")
-        # lines()
-        # print(f"Test dataset: {len(dataset)} Samples")
-        # print(f"Dictionary Size: {len(dictionary)}")
-        # starts()
-        # Set up dataloader
-        dataloader = TorchDataLoader(
-            dataset,
-            batch_size=CONFIG4RNN.PREPROCESSOR.BATCHES,
-            shuffle_state=CONFIG4RNN.PREPROCESSOR.SHUFFLE,
-            workers=CONFIG4RNN.PREPROCESSOR.WORKERS,
-        )
-        idx: int = randint(0, len(dataloader) - 1)
-        # print(dataloader[idx])
-
+        starts()
+        print("Data Preprocessing Summary:")
+        lines()
+        print(f"Chinese Dictionary Size: {len(dictionary_cn)} Samples")
+        print(f"English Dictionary Size: {len(dictionary_en)} Samples")
+        starts()
+        """
+        ****************************************************************
+        Data Preprocessing Summary:
+        ----------------------------------------------------------------
+        Chinese Dictionary Size: 5235 Samples
+        English Dictionary Size: 3189 Samples
+        ****************************************************************
+        """
         # Load the save model parameters
-        params: Path = Path(CONFIG4RNN.FILEPATHS.SAVED_NET)
+        options: list[str] = [CONFIG4RNN.FILEPATHS.TRAINED_NET_GREEDY, CONFIG4RNN.FILEPATHS.TRAINED_NET_BEAM]
+        selection: str = choice(options)
+        params: Path = Path(selection)
         if params.exists():
-            print(f"Model {params.name} Exists!")
+            print(f"Model {green(params.name)} Exists!")
 
             # Set up a model and load saved parameters
-            model = GRUClassifier(
-                len(dictionary),
+            model = SeqToSeqCoder(
+                len(dictionary_cn),
+                len(dictionary_en),
                 embedding_dim=CONFIG4RNN.PARAMETERS.EMBEDDING_DIM,
                 hidden_size=CONFIG4RNN.PARAMETERS.HIDDEN_SIZE,
                 num_layers=CONFIG4RNN.PARAMETERS.LAYERS,
-                num_classes=CONFIG4RNN.PARAMETERS.CLASSES,
                 dropout_rate=CONFIG4RNN.PREPROCESSOR.DROPOUT_RATIO,
-                accelerator=CONFIG4RNN.HYPERPARAMETERS.ACCELERATOR,
-                task=Tasks.CLASSIFICATION,
-                pad_idx=dictionary[Tokens.PAD]
+                bid=True,
+                pad_idx4input=dictionary_cn[Tokens.PAD],
+                pad_idx4output=dictionary_en[Tokens.PAD],
+                net_category=Seq2SeqNet.GRU,
+                SOS=dictionary_cn[Tokens.SOS],
+                EOS=dictionary_en[Tokens.EOS],
             )
             dict_state: dict = load(params, map_location=device(CONFIG4RNN.HYPERPARAMETERS.ACCELERATOR))
             model.load_state_dict(dict_state)
             model.eval()
             print("Model Loaded Successfully!")
 
-            # Predict
-            total = 0
-            correct_counter = 0
+            # Convert sentences to sequence using dictionary
+            sequences: list[list[int]] = build_word2id_seqs(cn_items, dictionary_cn, UNK=Tokens.UNK)
+            # print(sequences[:3])
 
+            # Predict and Evaluate
             with no_grad():
-                for X, y in tqdm(dataloader, total=len(dataloader), desc="Evaluating Trained Model:"):
-                    logits = model(X)
-                    # Convert the output to probabilities along the class dimension
-                    probabilities: Tensor = nn.functional.softmax(logits, dim=1)
-                    # Get predicted labels
-                    y_pred: Tensor = argmax(probabilities, dim=1)
+                strategy: str = (
+                    Seq2SeqStrategies.GREEDY
+                    if params.name.split(".")[0].split("-")[2] in str(CONFIG4RNN.FILEPATHS.TRAINED_NET_GREEDY)
+                    else Seq2SeqStrategies.BEAM_SEARCH
+                )
 
-                    # Compare with true labels
-                    correct: Tensor = (y_pred == y)
-
-                    # Update counters
-                    total += y.size(0)
-                    correct_counter += correct.sum().item()
-
-            # Accuracy
-            accuracy = correct_counter / total
-            starts()
-            print("Trained Model Evaluation")
-            lines()
-            print(f"Total Samples: {total}")
-            print(f"Correct Predictions: {correct_counter}")
-            print(f"Accuracy: {accuracy:.2%}")
-            starts()
-            """
-            ****************************************************************
-            Trained LSTM Model Evaluation
-            ----------------------------------------------------------------
-            Total Samples: 9448
-            Correct Predictions: 9440
-            Accuracy: 99.92%
-            ****************************************************************
-            ****************************************************************
-            Trained GRU Model Evaluation
-            ----------------------------------------------------------------
-            Total Samples: 9448
-            Correct Predictions: 9440
-            Accuracy: 99.92%
-            ****************************************************************
-            """
+                assert len(sequences) == len(en4prove), "src and truth tgt length mismatch."
+                results: list[tuple[list[str], list[str], float]] = []
+                for seq, reference in tqdm(zip(sequences, en_items), total=len(sequences), desc=f"{strategy} Model:"):
+                    src: Tensor = tensor(seq, dtype=long, device=device(CONFIG4RNN.HYPERPARAMETERS.ACCELERATOR))
+                    out = model.generate(src.unsqueeze(dim=0))
+                    pred = [reversed_dict.get(idx, Tokens.UNK) for idx in out.squeeze().tolist()]
+                    hypothesis: list[str] = [word.strip() for word in pred if word != Tokens.EOS]
+                    # print(hypothesis)
+                    # print(reference)
+                    bleu = bleu_score(reference, hypothesis)
+                    # print(f"BLEU Score: {bleu:.4f}")
+                    results.append((reference, hypothesis, bleu))
+                starts()
+                print(f"Evaluation Results for {strategy} Model:")
+                scores: list[float] = [score for _, _, score in results]
+                lines()
+                print(f"Sentence Amount: {len(scores)}")
+                print(f"Mean BLEU:       {np_mean(scores):.4f}")
+                print(f"Highest BLEU:    {np_max(scores):.4f}")
+                print(f"Lowest BLEU:     {np_min(scores):.4f}")
+                print(f"Median BLEU:     {np_median(scores):.4f}")
+                starts()
+                """
+                ****************************************************************
+                Evaluation Results for beam Model:
+                ----------------------------------------------------------------
+                Sentence Amount: 4374
+                Mean BLEU:       0.1637
+                Highest BLEU:    1.0000
+                Lowest BLEU:     0.0000
+                Median BLEU:     0.0845
+                ****************************************************************
+                Normal
+                ****************************************************************
+                Evaluation Results for greedy Model:
+                ----------------------------------------------------------------
+                Sentence Amount: 4374
+                Mean BLEU:       0.1637
+                Highest BLEU:    1.0000
+                Lowest BLEU:     0.0000
+                Median BLEU:     0.0845
+                ****************************************************************
+                """
         else:
             print(f"Model {params.name} does not exist!")
 
