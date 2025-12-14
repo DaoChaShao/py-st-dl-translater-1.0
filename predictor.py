d7b8254ae0801e142537a76ce9dca2196d8100e5
DaoChaShao <27882272@qq.com>
2025-12-14 14:35:56 +0800

    feat: implement database connection and enhance prediction with BLEU score evaluation

--- predictor.py
+++ predictor.py
@@ -6,32 +6,40 @@
 # @File     :   predictor.py
 # @Desc     :   
 
+from functools import cache
 from pathlib import Path
-from random import randint
-from re import search
-from torch import Tensor, load, device, no_grad, nn, tensor, long, argmax
+from random import randint, choice
+from torch import Tensor, load, device, no_grad
 
 from src.configs.cfg_rnn import CONFIG4RNN
-from src.configs.cfg_types import Lang, Tokens, Tasks
-from src.nets.gru4classification import GRUClassifier
-from src.utils.apis import OpenAITextCompleter
-from src.utils.helper import Timer, read_yaml
-from src.utils.highlighter import red, green, yellow, blue, purple
-from src.utils.nlp import spacy_batch_tokeniser
+from src.configs.cfg_types import Lang, Tokens, Seq2SeqNet, Seq2SeqStrategies
+from src.nets.seq2seq import SeqToSeqCoder
+from src.utils.helper import Timer
+from src.utils.highlighter import starts, lines, red, green, blue
+from src.utils.NLTK import bleu_score
+from src.utils.nlp import SpaCyBatchTokeniser, build_word2id_seqs
 from src.utils.PT import item2tensor
 from src.utils.SQL import SQLiteIII
 from src.utils.stats import create_full_data_split, load_json
 
 
+@cache
+def connect_db() -> list[tuple]:
+    """ Connect to the Database and Return the Connection Object """
+    table: str = "translater"
+    cols: dict = {"en": str, "cn": str}
+    path: Path = Path(CONFIG4RNN.FILEPATHS.SQLITE)
+
+    with SQLiteIII(table, cols, path) as db:
+        data = db.fetch_all(col_names=[col for col in cols.keys()])
+
+    return data
+
+
 def main() -> None:
     """ Main Function """
-    # Get the data from the database: Method II
-    table: str = "news"
-    cols: dict[str, type[int | str]] = {"label": int, "content": str}
-    with SQLiteIII(table, cols, CONFIG4RNN.FILEPATHS.SQLITE) as db:
-        database = db.fetch_all([col for col in cols.keys()])
-        # print(len(database))
-        # print()
+    # Get the data from the database
+    database: list[tuple] = connect_db()
     # pprint(data[:3])
     # print(len(data))
 
@@ -40,104 +48,124 @@ def main() -> None:
         _, _, data = create_full_data_split(database)
 
         # Tokenise the data
-        amount: int | None = 100
-        # amount: int | None = None
-        contents: list[str] = [content for _, content in data]
+        # amount: int | None = 100
+        amount: int | None = None
+        batches: int = 16 if amount else 128
+        cn4prove: list[str] = [c for _, c in data]
+        en4prove: list[str] = [e for e, _ in data]
+        assert len(cn4prove) == len(en4prove), "Chinese and English data length mismatch."
         if amount is None:
-            news: list[list[str]] = spacy_batch_tokeniser(
-                contents, lang=Lang.EN, batches=CONFIG4RNN.PREPROCESSOR.BATCHES
-            )
+            with SpaCyBatchTokeniser(Lang.CN, batches=batches, strict=False) as tokeniser:
+                cn_items: list[list[str]] = tokeniser.batch_tokenise(cn4prove)
+            with SpaCyBatchTokeniser(Lang.EN, batches=batches, strict=False) as tokeniser:
+                en_items: list[list[str]] = tokeniser.batch_tokenise(en4prove)
         else:
-            news: list[list[str]] = spacy_batch_tokeniser(
-                contents[:amount], lang=Lang.EN, batches=CONFIG4RNN.PREPROCESSOR.BATCHES
-            )
-        # print(news)
+            with SpaCyBatchTokeniser(Lang.CN, batches=batches, strict=False) as tokeniser:
+                cn_items: list[list[str]] = tokeniser.batch_tokenise(cn4prove[:amount])
+            with SpaCyBatchTokeniser(Lang.EN, batches=batches, strict=False) as tokeniser:
+                en_items: list[list[str]] = tokeniser.batch_tokenise(en4prove[:amount])
+        # print(cn_items[:3])
+        # print(en_items[:3])
 
-        # Load the dictionary and convert
-        dic: Path = Path(CONFIG4RNN.FILEPATHS.DICTIONARY)
-        dictionary: dict[str, int] = load_json(dic)
+        # Load dictionary
+        dic_cn: Path = Path(CONFIG4RNN.FILEPATHS.DICTIONARY_CN)
+        dictionary_cn: dict = load_json(dic_cn) if dic_cn.exists() else print("Dictionary file not found.")
+        dic_en: Path = Path(CONFIG4RNN.FILEPATHS.DICTIONARY_EN)
+        dictionary_en: dict = load_json(dic_en) if dic_en.exists() else print("Dictionary file not found.")
+        reversed_dict: dict = {idx: word for word, idx in dictionary_en.items()}
+        # print(reversed_dict)
 
-        # Convert a random sentence to sequence using dictionary
-        idx: int = randint(0, len(news) - 1)
-        sequence: list[str] = news[idx]
-        seq: list[int] = [dictionary.get(item, Tokens.UNK) for item in sequence]
-        # print(seq)
-
-        # Convert the token to a tensor
-        X: Tensor = item2tensor(seq, embedding=True, accelerator=CONFIG4RNN.HYPERPARAMETERS.ACCELERATOR)
-        # Add batch size
-        X = X.unsqueeze(0)
-        # print(X)
-        # Get the relevant label
-        labels: list[int] = [label for label, _ in data]
-        y: Tensor = item2tensor(labels[idx], accelerator=CONFIG4RNN.HYPERPARAMETERS.ACCELERATOR)
-        print(y)
+        starts()
+        print("Data Preprocessing Summary:")
+        lines()
+        print(f"Chinese Dictionary Size: {len(dictionary_cn)} Samples")
+        print(f"English Dictionary Size: {len(dictionary_en)} Samples")
+        starts()
+        """
+        ****************************************************************
+        Data Preprocessing Summary:
+        ----------------------------------------------------------------
+        Chinese Dictionary Size: 5235 Samples
+        English Dictionary Size: 3189 Samples
+        ****************************************************************
+        """
 
         # Load the save model parameters
-        params: Path = Path(CONFIG4RNN.FILEPATHS.SAVED_NET)
+        options: list[str] = [CONFIG4RNN.FILEPATHS.TRAINED_NET_GREEDY, CONFIG4RNN.FILEPATHS.TRAINED_NET_BEAM]
+        selection: str = choice(options)
+        params: Path = Path(selection)
         if params.exists():
-            print(f"Model {params.name} Exists!")
+            print(f"Model {green(params.name)} Exists!")
 
             # Set up a model and load saved parameters
-            model = GRUClassifier(
-                len(dictionary),
+            model = SeqToSeqCoder(
+                len(dictionary_cn),
+                len(dictionary_en),
                 embedding_dim=CONFIG4RNN.PARAMETERS.EMBEDDING_DIM,
                 hidden_size=CONFIG4RNN.PARAMETERS.HIDDEN_SIZE,
                 num_layers=CONFIG4RNN.PARAMETERS.LAYERS,
-                num_classes=CONFIG4RNN.PARAMETERS.CLASSES,
                 dropout_rate=CONFIG4RNN.PREPROCESSOR.DROPOUT_RATIO,
-                accelerator=CONFIG4RNN.HYPERPARAMETERS.ACCELERATOR,
-                task=Tasks.CLASSIFICATION,
-                pad_idx=dictionary[Tokens.PAD]
+                bid=True,
+                pad_idx4input=dictionary_cn[Tokens.PAD],
+                pad_idx4output=dictionary_en[Tokens.PAD],
+                net_category=Seq2SeqNet.GRU,
+                SOS=dictionary_cn[Tokens.SOS],
+                EOS=dictionary_en[Tokens.EOS],
             )
             dict_state: dict = load(params, map_location=device(CONFIG4RNN.HYPERPARAMETERS.ACCELERATOR))
             model.load_state_dict(dict_state)
             model.eval()
             print("Model Loaded Successfully!")
 
+            # Convert sentences to sequence using dictionary
+            sequences: list[list[int]] = build_word2id_seqs(cn_items, dictionary_cn, UNK=Tokens.UNK)
+            # print(sequences[:3])
+
+            # Randomly select a data point for prediction
+            assert len(sequences) == len(en_items), "src and truth tgt length mismatch."
+            idx: int = randint(0, len(sequences) - 1)
+            seq: list[int] = sequences[idx]
+            # Convert the token to a tensor
+            src: Tensor = item2tensor(seq, embedding=True, accelerator=CONFIG4RNN.HYPERPARAMETERS.ACCELERATOR)
+            # Add batch size
+            src = src.unsqueeze(0)
+            # print(src.shape, src)
+
             # Prediction
             with no_grad():
-                logits: Tensor = model(X)
-                # print(logist)
-                probabilities: Tensor = nn.functional.softmax(logits)
-                # print(probabilities)
-                y_pred: Tensor = argmax(probabilities, dim=1)
-                print(y_pred)
+                strategy: str = (
+                    Seq2SeqStrategies.GREEDY
+                    if params.name.split(".")[0].split("-")[2] in str(CONFIG4RNN.FILEPATHS.TRAINED_NET_GREEDY)
+                    else Seq2SeqStrategies.BEAM_SEARCH
+                )
 
-                correct: bool = (y_pred.item() == y.item())
-                # print(correct)
-                print(blue("Positive") if y == 2 else purple("Negative") if y == 1 else yellow("Neutral"))
+                out: Tensor = model.generate(src)
+                pred = [reversed_dict.get(idx, Tokens.UNK) for idx in out.squeeze().tolist()]
+                hypothesis: list[str] = [word.strip() for word in pred if word != Tokens.EOS]
+                # Get the relevant reference
+                reference: list[str] = en_items[idx]
 
-                rate: str = green("Bingo") if correct == True else red("Damn")
-                print(f"The content of the selected news {idx}:\n{contents[idx]}")
-                print(f"The prediction of the news {idx} is {rate} !")
-
-            # Prompt Engineering with OpenAI API
-            key: Path = Path(CONFIG4RNN.FILEPATHS.API_KEY)
-            config: dict = read_yaml(key)
-            API_KEY: str = config["openai"]["api_key"]
-            opener = OpenAITextCompleter(API_KEY, temperature=0)
-            role: str = "You are a professional financial expert with cross-cultural expertise."
-            rating: str = """
-                        0: Neutral Review
-                        1: Negative Review
-                        2: Positive Review
-                        """
-            prompt: str = f"""
-                        Give a brief explanation in Chinese after reading the following review:
-                        {contents[idx]}.
-                        Please analyze it, give a reason, and provide a rating (Only return number) as follows:
-                        {rating}.
-                        """
-            explanation = opener.client(role, prompt)
-            print(explanation)
-
-            match = search(r"\b[0-2]\b", explanation)
-            pred_label: int | None = int(match.group()) if match else None
-            print(f"Predicted Label from OpenAI: {pred_label}")
-
-            result = green("Bingo") if (pred_label == y_pred) else red("Damn")
-            print(f"The prediction between OpenAI and RNN Model: {result} !")
+                bleu = bleu_score(reference, hypothesis)
+                starts()
+                print(f"Evaluation Results for {strategy} Model:")
+                lines()
+                print(f"Selected Data Index for Prediction: {red(str(idx))}")
+                print(f"Input Sentence (CN):                {cn4prove[idx]}")
+                print(f"Reference (EN):                     {reference}")
+                print(f"Predation (EN):                     {hypothesis}")
+                print(f"BLEU Score:                         {blue(f"{bleu:.4f}")}")
+                starts()
+                """
+                ****************************************************************
+                Evaluation Results for beam Model:
+                ----------------------------------------------------------------
+                Selected Data Index for Prediction: 2102
+                Input Sentence (CN):                他喜欢听收音机。
+                Reference (EN):                     ['he', 'like', 'listen', 'to', 'the', 'radio', '.']
+                Predation (EN):                     ['he', 'like', 'to', 'listen', 'to', 'the', 'radio', '.']
+                BLEU Score:                         0.5946
+                ****************************************************************
+                """
         else:
             print(f"Model {params.name} does not exist!")
 
